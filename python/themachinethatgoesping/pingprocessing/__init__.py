1b8f04eb2f6c814735e7d7b563b24ec6c3343c11
Peter Urban <peter.urban@ugent.be>
2023-10-16 13:20:42 +0200

    properly overwrite modules

--- python/themachinethatgoesping/pingprocessing/__init__.py
+++ python/themachinethatgoesping/pingprocessing/__init__.py
@@ -2,12 +2,15 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+# folders
+from . import watercolumn as watercolumn_ext  # flake8: noqa
+
+# modules
+
 #cpp modules
 from themachinethatgoesping.pingprocessing_cppy import *  # flake8: noqa
 
-# folders
-from . import watercolumn
-
-# modules
+# overwrite timeconv module using the loaded python extension
+watercolumn = watercolumn_ext
 
 __version__ = "@PROJECT_VERSION@"
