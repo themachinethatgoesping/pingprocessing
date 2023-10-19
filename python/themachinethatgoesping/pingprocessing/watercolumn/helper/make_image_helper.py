150c1d8ddac35de087cea7a02732168ee7f79aa0
Peter Urban <peter.urban@ugent.be>
2023-10-19 12:05:07 +0200

    enh get_bottom_directions_wci uses bottom detections - if available and bottome_detection_sample_number > 0

--- python/themachinethatgoesping/pingprocessing/watercolumn/helper/make_image_helper.py
+++ python/themachinethatgoesping/pingprocessing/watercolumn/helper/make_image_helper.py
@@ -1,51 +1,57 @@
-"""
-functions for making images from watercolumn data
-"""
-
 import numpy as np
-
 import themachinethatgoesping.echosounders as es
 import themachinethatgoesping.algorithms.geoprocessing as gp
 
-def get_bottom_directions_wci(ping : es.filetemplates.I_Ping, 
-                              selection : es.pingtools.BeamSelection = None) -> (gp.datastructures.XYZ_1, gp.datastructures.SampleDirectionsRange_1, np.ndarray):
-    """retrieve bottom positions/directions/sample numbers from a water column ping
-    Note: this function is an approximation (for performance reasons). As such it 
-    assumes a constant sound velocity profile
+def get_bottom_directions_wci(
+    ping: es.filetemplates.I_Ping, 
+    selection: es.pingtools.BeamSelection = None) -> (gp.datastructures.XYZ_1, gp.datastructures.SampleDirectionsRange_1, np.ndarray):
+    """
+    Retrieve bottom positions/directions/sample numbers from a water column ping.
+    Note: this function is an approximation (for performance reasons). As such, it 
+    assumes a constant sound velocity profile.
 
     Parameters
     ----------
     ping : es.filetemplates.I_Ping
-        Ping to retrieve bottom positions/directions/sample numbers from
+        Ping to retrieve bottom positions/directions/sample numbers from.
     selection : es.pingtools.BeamSelection, optional
-        A beam selection to retreive the directions from, by default None
+        A beam selection to retrieve the directions from, by default None.
 
     Returns
     -------
-    _type_
-        _description_
+    Tuple[gp.datastructures.XYZ_1, gp.datastructures.SampleDirectionsRange_1, np.ndarray]
+        A tuple containing the bottom positions, directions, and sample numbers.
     """
     
-    # create select all selection if necessary
+    # Create select all selection if necessary.
     if selection is None:
         selection = ping.watercolumn.get_beam_sample_selection_all()
         
-    # get sensor configuration1450
+    # Get sensor configuration.
     sc = ping.get_sensor_configuration()
     pingoff = sc.get_target("Transducer")
     posoff = sc.get_position_source()
     geolocation = ping.get_geolocation()
-    bottom_direction_sample_numbers = ping.watercolumn.get_number_of_samples_per_beam(selection)
+
+    if ping.watercolumn.has_bottom_range_samples():
+        bottom_direction_sample_numbers = ping.watercolumn.get_bottom_range_samples(selection)
+
+        # If the bottom direction sample numbers are 0, set them to the number of samples per beam.
+        bottom_direction_sample_numbers_is_0 = np.nonzero(bottom_direction_sample_numbers == 0)
+        if len(bottom_direction_sample_numbers_is_0) > 0:
+            bottom_direction_sample_numbers[bottom_direction_sample_numbers_is_0] = ping.watercolumn.get_number_of_samples_per_beam(selection)[bottom_direction_sample_numbers_is_0]
+    else:
+        bottom_direction_sample_numbers = ping.watercolumn.get_number_of_samples_per_beam(selection)
     
     bottomdirections = gp.datastructures.SampleDirectionsTime_1([selection.get_number_of_beams()])
     bottomdirections.crosstrack_angle = ping.watercolumn.get_beam_crosstrack_angles(selection) - geolocation.roll
     bottomdirections.alongtrack_angle = ping.watercolumn.get_beam_alongtrack_angles(selection)
     bottomdirections.two_way_travel_time = bottom_direction_sample_numbers * ping.watercolumn.get_sample_interval()
 
-    #TODO: get sound velocity from SVP
+    # TODO: Get sound velocity from SVP.
     c = 1450
 
-    # raytrace to bottom assuming constant sound velocity profile
+    # Raytrace to bottom assuming constant sound velocity profile.
     rt = gp.raytracers.RTConstantSVP(geolocation, c)
     xyz = rt.trace_points(bottomdirections)
     bottom_directions = gp.datastructures.SampleDirectionsRange_1([ping.watercolumn.get_number_of_beams()])
@@ -55,7 +61,20 @@ def get_bottom_directions_wci(ping : es.filetemplates.I_Ping,
     
     return xyz, bottom_directions, bottom_direction_sample_numbers
 
-def get_bottom_directions_bottom(ping):
+def get_bottom_directions_bottom(ping: es.filetemplates.I_Ping) -> (gp.datastructures.XYZ_1, gp.datastructures.SampleDirectionsRange_1, np.ndarray):
+    """
+    Retrieve bottom positions/directions/sample numbers from a bottom ping.
+
+    Parameters
+    ----------
+    ping : es.filetemplates.I_Ping
+        Ping to retrieve bottom positions/directions/sample numbers from.
+
+    Returns
+    -------
+    Tuple[gp.datastructures.XYZ_1, gp.datastructures.SampleDirectionsRange_1, np.ndarray]
+        A tuple containing the bottom positions, directions, and sample numbers.
+    """
     sc = ping.get_sensor_configuration()
     pingoff = sc.get_target("Transducer")
     posoff = sc.get_position_source()
@@ -71,3 +90,5 @@ def get_bottom_directions_bottom(ping):
     bottom_direction_sample_numbers = ping.watercolumn.get_bottom_sample_numbers()
     
     return xyz, bottom_directions, bottom_direction_sample_numbers
+
+
