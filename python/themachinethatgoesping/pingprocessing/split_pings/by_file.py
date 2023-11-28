8ed7e634420f537d03d0c1f3078dee278dcfbc94
Peter Urban <peter.urban@ugent.be>
2023-11-28 20:43:43 +0100

    add split_pings.by_file_path function

--- python/themachinethatgoesping/pingprocessing/split_pings/by_file.py
+++ python/themachinethatgoesping/pingprocessing/split_pings/by_file.py
@@ -9,9 +9,9 @@ from themachinethatgoesping.echosounders import filetemplates
 I_Ping = filetemplates.I_Ping
 
 
-def by_channel_id(pings: List[I_Ping], progress: bool = False) -> Dict[int, List[I_Ping]]:
+def by_file_nr(pings: List[I_Ping], progress: bool = True) -> Dict[int, List[I_Ping]]:
     """
-    Splits a list of pings by channel id.
+    Splits a list of pings by the associated file number.
 
     Args:
         pings (List[I_Ping]): A list of pings to be split.
@@ -25,21 +25,27 @@ def by_channel_id(pings: List[I_Ping], progress: bool = False) -> Dict[int, List
     split_pings = defaultdict(list)
 
     for ping in it:
-        split_pings[ping.get_channel_id()].append(ping)
+        split_pings[ping.get_file_nr()].append(ping)
 
     return split_pings
-def by_channel_id(
-    pings: List[I_Ping], 
-    progress: bool = False) -> Dict[int, List[I_Ping]]:
-    
-    it = get_progress_iterator(pings, progress, desc = "Split pings by channel id")
+
+def by_file_path(pings: List[I_Ping], progress: bool = True) -> Dict[str, List[I_Ping]]:
+    """
+    Splits a list of pings by the associated file path.
+
+    Args:
+        pings (List[I_Ping]): A list of pings to be split.
+        progress (bool, optional): Whether to show a progress bar. Defaults to False.
+
+    Returns:
+        Dict[str, List[I_Ping]]: A dictionary where the keys are channel ids and the values are lists of pings.
+    """
+    it = get_progress_iterator(pings, progress, desc="Split pings by channel id")
 
     split_pings = defaultdict(list)
 
     for ping in it:
-        split_pings[ping.get_channel_id()].append(ping)
+        split_pings[ping.get_file_path()].append(ping)
 
     return split_pings
-
-
 
