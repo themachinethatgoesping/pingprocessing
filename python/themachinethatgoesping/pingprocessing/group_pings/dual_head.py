5af2da3c6e0216de53230d9794593a29fbf5a9ef
Peter Urban <peter.urban@ugent.be>
2023-12-16 00:17:23 +0100

    ping_groups: sort by time - maybe this one will be reverted ...

--- python/themachinethatgoesping/pingprocessing/group_pings/dual_head.py
+++ python/themachinethatgoesping/pingprocessing/group_pings/dual_head.py
@@ -2,6 +2,7 @@ from collections import defaultdict, OrderedDict
 from typing import Dict, List
 from themachinethatgoesping.echosounders import filetemplates 
 from themachinethatgoesping.pingprocessing.core.progress import get_progress_iterator
+import numpy as np
 
 # Import necessary modules and types
 
@@ -22,16 +23,27 @@ def dual_head(pings: List[filetemplates.I_Ping], progress: bool = False) -> List
     """
 
     it = get_progress_iterator(pings, progress, desc="Group dual head pings")
-
+    
     ping_groups = []
     ping_group_map = {}
     for ping in it:
+        
         key = (ping.file_data.get_primary_file_nr(), ping.file_data.get_file_ping_counter())
 
-        if key not in ping_group_map:
+        if key not in ping_group_map.keys():
             ping_group_map[key] = len(ping_groups)
             ping_groups.append(OrderedDict())
 
         ping_groups[ping_group_map[key]][ping.get_channel_id()] = ping
-
-    return ping_groups
+        
+    # get times for each group
+    # TODO: this should not be necessary ... 
+    pg_times = []
+    for ping_group in ping_groups:
+        times=[]
+        for k,v in ping_group.items():
+            times.append(v.get_timestamp())
+        pg_times.append(np.nanmin(times))
+              
+    # return sorted by times
+    return np.array(ping_groups)[np.argsort(pg_times)]
