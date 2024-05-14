7f70912211a2a1771c3cac9fd7acb3f73753674f
Peter Urban <peter.urban@ugent.be>
2024-05-14 12:34:44 +0200

    add custom jupyter tqdm widget (intendet for internal use)

---
+++ python/themachinethatgoesping/pingprocessing/widgets/tqdmwidget.py
@@ -0,0 +1,46 @@
+from collections import OrderedDict
+from ipywidgets import IntProgress
+
+class TqdmWidget(IntProgress):
+    def __init__(self, **kwargs):
+        _kwargs = {
+            "value" : 0,
+            "min" : 0,
+            "max" : 10,
+            "step" : 1,
+            "description" : "Idle",
+            "orientation" : "horizontal",
+        }
+        _kwargs.update(kwargs)
+        super().__init__(**_kwargs)
+        
+    def __call__(self, list_like):
+        self.list_like = list_like
+        self.list_iter = iter(list_like)
+        self.index = 0
+        self.total = len(list_like)
+
+        #IntProgress values
+        self.max = self.total
+        self.value = 0
+        
+        return self
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.index += 1
+        self.value = self.index
+        return next(self.list_iter)
+    
+    def __len__(self):
+        return self.total
+    
+    def update(self):
+        self.index += 1
+        next(self.list_iter)
+        self.value = self.index
+        
+    def close(self):
+        pass
