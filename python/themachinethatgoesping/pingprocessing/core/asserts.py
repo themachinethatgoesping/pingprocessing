783dfe58f2eeed0eabb2eeaf761e7741a210cdb3
Peter Urban <peter.urban@ugent.be>
2024-12-13 12:14:47 +0100

    fixed ineffective assert

--- python/themachinethatgoesping/pingprocessing/core/asserts.py
+++ python/themachinethatgoesping/pingprocessing/core/asserts.py
@@ -1,6 +1,6 @@
 def assert_length(function_name, base_vector, compare_vectors):
     for vec in compare_vectors:
-        len(vec) == len(base_vector), f"ERROR[{function_name}]: error size mismatch! {len(vec)} != {len(base_vector)}"
+        assert len(vec) == len(base_vector), f"ERROR[{function_name}]: error size mismatch! {len(vec)} != {len(base_vector)}"
 
 
 def assert_valid_argument(function_name, arg, valid_args):
