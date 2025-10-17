c4eb67189a78a5d9ac18987718a3fd9fdd44b1f6
Peter Urban <peter.urban@ugent.be>
2025-10-17 22:47:43 +0200

    update pch (win fix)

--- src/pch/pingprocessing_pingprocessing_nanopy.hpp
+++ src/pch/pingprocessing_pingprocessing_nanopy.hpp
@@ -2,4 +2,6 @@
 // Target: pingprocessing:pingprocessing_nanopy
 #pragma once
 
-#include <nanobind/nanobind.h>
+#if !defined(__WIN32__) && !defined(_WIN32) && !defined(__WIN64__) && !defined(_WIN64)
+    #include <nanobind/nanobind.h>
+#endif
